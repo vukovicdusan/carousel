04d2b7cf30d70eb43fa1c7a7b0371936252f69f8
Picvajzmen <picvajzmen@gmail.com>
2021-10-11 11:47:15 +0200

    oja varijanta sa opacityjem

--- script.js
+++ script.js
@@ -3,55 +3,20 @@ const slides = document.querySelectorAll(".carousel__photo");
 const next = document.querySelector(".carousel__button--next");
 const prev = document.querySelector(".carousel__button--prev");
 //line slides up
-const slideWidth = slides[0].getBoundingClientRect().width;
-slides.forEach((slide, index) => {
-  slide.style.left = slideWidth * index + "px";
-});
-
-// function slideMover(currentSlide, targetSlide) {
-//   let amountToMove = targetSlide.style.left;
-//   track.style.transform = "translate(-" + amountToMove + ")";
-//   currentSlide.classList.remove("current");
-//   targetSlide.classList.add("current");
-
-//   if (slides[0].classList.contains("current")) {
-//     prev.classList.add("no-see");
-//   } else if (slides[slides.length - 1].classList.contains("current")) {
-//     next.classList.add("no-see");
-//   } else {
-//     prev.classList.remove("no-see");
-//     next.classList.remove("no-see");
-//   }
-// }
-
-//asign the clicks
+// const slideWidth = slides[0].getBoundingClientRect().width;
+// slides.forEach((slide, index) => {
+//   slide.style.left = slideWidth * index + "px";
+// });
+// console.log(slideWidth);
 next.addEventListener("click", (e) => {
   let current = document.querySelector(".current");
   let next = current.nextElementSibling;
-  let nextMove = next.style.left;
-  // let currentMove = slideWidth * (slides.length - 1);
-  next.style.transform = "translate(-" + nextMove + ")";
-  current.classList.add("previous-current");
-  current.classList.remove("current");
-  let previousCurrent = document.querySelector(".previous-current");
-  //call new function for add/remove and move
-  addRemoveAndMove(previousCurrent);
 
-  // previousCurrent.style.transform = "translate(" + currentMove + ")";
-  next.classList.add("current");
-  console.log(nextMove);
-  // previousCurrent.classList.remove(".previous-current");
-});
-
-function addRemoveAndMove(element) {
-  let amountToMove = slideWidth * (slides.length - 1);
-  element.style.transform = "translate(" + amountToMove + "px)";
-  element.classList.remove("previous-current");
-  console.log(amountToMove);
-}
-
-prev.addEventListener("click", (e) => {
-  let current = document.querySelector(".current");
-  let prev = current.previousElementSibling;
-  // slideMover(current, prev);
+  if (slides[slides.length - 1].classList.contains("current")) {
+    slides[0].classList.add("current");
+    slides[slides.length - 1].classList.remove("current");
+  } else {
+    current.classList.remove("current");
+    next.classList.add("current");
+  }
 });
