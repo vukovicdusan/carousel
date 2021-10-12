643db3102cd0649b06e790c5c55e95783cc90019
unknown <picvajzmen@gmail.com>
2021-10-11 23:20:58 -0700

    pocetak neke animacije..

--- script.js
+++ script.js
@@ -2,15 +2,11 @@ const track = document.querySelector(".carousel-track");
 const slides = document.querySelectorAll(".carousel__photo");
 const next = document.querySelector(".carousel__button--next");
 const prev = document.querySelector(".carousel__button--prev");
-//line slides up
-// const slideWidth = slides[0].getBoundingClientRect().width;
-// slides.forEach((slide, index) => {
-//   slide.style.left = slideWidth * index + "px";
-// });
-// console.log(slideWidth);
+
 next.addEventListener("click", (e) => {
   let current = document.querySelector(".current");
   let next = current.nextElementSibling;
+ 
 
   if (slides[slides.length - 1].classList.contains("current")) {
     slides[0].classList.add("current");
@@ -18,5 +14,28 @@ next.addEventListener("click", (e) => {
   } else {
     current.classList.remove("current");
     next.classList.add("current");
+  }
+  animate(next);
+});
+
+const animate = (element) => {
+  element.animate([
+    {transform: 'translate(100%)'},
+    {transform: 'translate(0)'}
+  ], {
+    duration: 1000,
+  });
+}
+
+prev.addEventListener("click", e => {
+  let current = document.querySelector(".current");
+  let prev = current.previousElementSibling;
+
+  if(slides[0].classList.contains("current")){
+    slides[slides.length-1].classList.add("current");
+    slides[0].classList.remove("current");
+  } else {
+    current.classList.remove("current");
+    prev.classList.add("current");
   }
 });
